2b096cde49934c5d00b9744d0ba37d02de2fdbb8
Nandeeshmern <nandeesh.ns1994@gmail.com>
2019-09-23 17:14:40 +0530

    Project1modified on 2309

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import img from './images/backgroud.jpg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import FacebookLogin from 'react-facebook-login';
+
+import GoogleLogin from 'react-google-login';
+
+class App extends Component {
+
+  render() {
+
+    const responseFacebook = (response) => {
+      console.log(response);
+    }
+
+    const responseGoogle = (response) => {
+      console.log(response);
+    }
+
+    return (
+      <center>
+
+        <div className="App">
+          <h1>LOGIN WITH FACEBOOK AND GOOGLE</h1>
+
+
+          <div className="buttons">
+            <FacebookLogin
+              appId="912664192439199"// facebook app id created by developer-facebook-login
+              fields="name,email,picture"
+              callback={responseFacebook}
+            />
+            <br />
+            <br />
+              <GoogleLogin
+              clientId="1078052189000-td7om4iah6v0n2ve40lvn6s0glukovmu.apps.googleusercontent.com"//google app id created by developer-google-login
+              buttonText="LOGIN WITH GOOGLE"
+              onSuccess={responseGoogle}
+              onFailure={responseGoogle}
+            />
+          </div>
+
+        </div>
+        <div className="picture">
+          <ul>
+            <img src="backgroud.jpg" />
+          </ul>
+        </div>
+      </center>
+    );
+  }
 }
 
 export default App;
